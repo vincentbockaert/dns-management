7b6b43d0b2382076a15af4cedcf69f183258003d
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2023-02-01 18:27:20 +0100

    SimpleLogin records for vincentbockaert.xyz

--- zones/vincentbockaert.xyz.js
+++ zones/vincentbockaert.xyz.js
@@ -1,5 +1,6 @@
 D("vincentbockaert.xyz", REG_NONE, DnsProvider(DSP_CLOUDFLARE),
     DefaultTTL("1h"),
     TXT("_keybase", "keybase-site-verification=mXZXyWy_FY8p2ppk-LtNZIzEb8Ak0JyVcLVK5cJ0OfY"),
-    includeFastmailMailRecords()
+    includeFastmailMailRecords(),
+    inncludeSimpeLogin()
 );
