d05e24c829d12603309e1d0b7b442ec608c015e8
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2023-02-01 18:38:07 +0100

    FIx, return of records in function

--- providers/mail.js
+++ providers/mail.js
@@ -1,5 +1,5 @@
 function includeTutanotaMailRecords() {
-    var l = [
+    var records = [
         MX("@", 0, "mail.tutanota.de."),
         TXT("@", "t-verify=37e68e99bac1229a43bb5ea1ccaa6668"),
         TXT("@", "v=spf1 include:spf.tutanota.de -all"),
@@ -9,25 +9,27 @@ function includeTutanotaMailRecords() {
         CNAME("s1._domainkey", "s1.domainkey.tutanota.de."),
         CNAME("s2._domainkey", "s2.domainkey.tutanota.de.")
     ]
-    return l
+    return records
 }
 
-function inncludeSimpeLogin() {
+function includeSimpeLogin() {
 
     var sl = "simplelogin";
     var base = ".vincentbockaert.xyz.";
 
-    var l = [
+    var records = [
         MX(sl,10,"app."+sl+base),
         A("app."+sl,"45.79.251.178"),
         TXT("dkim._domainkey."+sl,"v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA8wIm5EMmhzxjICqCANVpUejgTAfsRyFUL4hRi22v8Ols0fUutlt26AK68gkyVrGFG2edOV4S9/zH1gROwBWy5yE+gRkwNsKncWpVlVpkdJPYIGVKiF2fq29cOYNC6Q44BjEdziX9SexRxKulnNbpvPUX8TB06wcxWLkUNce62Oox66qkPfc6nCuzJBk8oAGerR0NPO8TqUAHZhtvM1q13pvkezklsQGq2WTHaS4uW/0LAf+D4toKUhN7n3oe7nXLPWlkxamK41QipbGMrD3kQt2ZuHnsOThaWzN3sQlFs35sbEpit9oDTEkUXO6Bd/p7BLRpbfvNdbW8bpzpMAeXfwIDAQAB"),
         TXT(sl,"v=spf1 mx ~all"),
         TXT("_dmarc."+sl,"v=DMARC1; p=quarantine; adkim=r; aspf=r")
     ]
+
+    return records
 }
 
 function includeFastmailMailRecords() {
-    var l = [
+    var records = [
         // mx
         MX("@", 10, "in1-smtp.messagingengine.com."),
         MX("@",20,"in2-smtp.messagingengine.com."),
@@ -38,5 +40,5 @@ function includeFastmailMailRecords() {
         // spf
         TXT("@","v=spf1 include:spf.messagingengine.com -all")
     ]
-    return l
+    return records
 }
