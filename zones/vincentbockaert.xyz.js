33771fe47cc6bab118a978277a3875b6fd45e43e
Vincent Bockaert <bockaert.vincent1997@gmail.com>
2023-05-31 23:32:22 +0200

    Switch to protonmail as mail provider

--- zones/vincentbockaert.xyz.js
+++ zones/vincentbockaert.xyz.js
@@ -1,6 +1,6 @@
 D('vincentbockaert.xyz', REG_NONE, DnsProvider(DSP_CLOUDFLARE),
     DefaultTTL("1h"),
     TXT("_keybase", "keybase-site-verification=mXZXyWy_FY8p2ppk-LtNZIzEb8Ak0JyVcLVK5cJ0OfY"),
-    includeFastmailMailRecords(),
-    includeSimpeLogin()
+    TXT("@", "protonmail-verification=71e8db583a7c5848b0bf82278e78278cc3ca20d7"),
+    includeProtonMailRecords()
 );
